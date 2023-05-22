be06b56f87ab95eb7ccad4e17b1b2f76c819473b
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-05-22 10:03:58 +0200

    Renommer les indexes pour pouvoir les trouver facilement

--- database/create_indexes.sql
+++ database/create_indexes.sql
@@ -1,20 +1,20 @@
 ------------------------------------------------------------------------------------------------------------------------
 -- Indexes pour les skyplot de cs
 
-create index concurrently "skyplot-cs1"
+create index concurrently "speed-skyplot-cs1"
 on skyplot(station_id)
 where cs1;
 
-create index concurrently "skyplot-cs2"
+create index concurrently "speed-skyplot-cs2"
 on skyplot(station_id)
 where cs2;
 
-create index concurrently "skyplot-cs5"
+create index concurrently "speed-skyplot-cs5"
 on skyplot(station_id)
 where cs5;
 
 ------------------------------------------------------------------------------------------------------------------------
 -- Indexes pour les autres skyplots
 
-create index concurrently "skyplot-station-date"
+create index concurrently "speed-skyplot-station-date"
 on skyplot(station_id, date_id);
