68f93acdda3c57b5a7f94973cefe5bbc7ee0d394
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-05-31 16:01:06 +0200

    Transformer certains test en test parametrisé

--- xtr2database/tests/test_extractors.py
+++ xtr2database/tests/test_extractors.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from pathlib import Path
 
 import pytest
@@ -7,16 +7,31 @@ from xtr2database.extractors import (get_file_date, get_station_coords,
                                      get_station_id)
 
 
-def test_get_file_date():
-    filename = "ADER00FRA-2023-01-02"
-    expected_date = datetime(2023, 1, 2).date()
+@pytest.mark.parametrize(
+    "file_stem, expected",
+    [
+        ("ADER00FRA-2023-01-02", date(2023, 1, 2)),
+        ("ADER00FRA-2023-12-31", date(2023, 12, 31)),
+        ("ADER00FRA-2023-05-31", date(2023, 5, 31)),
+        ("BOUF00FRA-2023-05-01", date(2023, 5, 1))
+    ]
+)
+def test_get_file_date(file_stem, expected):
+    assert get_file_date(file_stem) == expected
 
-    assert get_file_date(filename) == expected_date
 
-
-def test_get_file_date_bad_arguments():
+@pytest.mark.parametrize(
+    "file_stem",
+    [
+        "file-2023-31-05.xtr",
+        "file-2023-ss",
+        "file-2023-05-ss",
+        "file-2023-05-ss.xtr"
+    ]
+)
+def test_get_file_date_bad_arguments(file_stem):
     with pytest.raises(ValueError):
-        get_file_date("file-2023-31-05.xtr")
+        get_file_date(file_stem)
 
 
 def test_get_station_id():
