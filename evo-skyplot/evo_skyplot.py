1ca72176cc680f082698968a96fb4b3b1f8fb2ec
Raphaël C <raphael.caldwell@etu.unice.fr>
2023-06-28 15:32:31 +0200

    Plus de validation de données

--- evo-skyplot/evo_skyplot.py
+++ evo-skyplot/evo_skyplot.py
@@ -1,17 +1,24 @@
 import asyncio
-from tempfile import TemporaryDirectory
-from urllib.parse import parse_qs, urlencode, urlparse, ParseResult
+import re
 from datetime import datetime, timedelta
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from urllib.parse import ParseResult, parse_qs, urlencode, urlparse
 
-from tqdm.asyncio import tqdm_asyncio
 import aiohttp
+from tqdm.asyncio import tqdm_asyncio
+
+URL_REGEX = re.compile("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+VALID_DATE_REGEX = re.compile(r"\d{4}-\d{2}-\d{2}")
 
 here = Path(__file__).parent
 semaphore = asyncio.Semaphore(5)
 
 
 def daterange(start_date: datetime, end_date: datetime):
+    """
+    Renvoie une liste de dates entre `start_date` et `end_date`
+    """
     out = []
     for n in range(int((end_date - start_date).days) + 1):
         out.append((start_date + timedelta(n)).strftime("%Y-%m-%d"))
@@ -19,12 +26,19 @@ def daterange(start_date: datetime, end_date: datetime):
 
 
 async def download_image(sess: aiohttp.ClientSession, parse_res: ParseResult, query: dict, i: int, date: datetime, dest: Path):
+    """
+    Télécharge une image de façon asynchrone et l'enregistre dans le dossier `dest`
+    """
     query["var-day"] = [date]
     url = parse_res.scheme + "://" + parse_res.netloc + parse_res.path + "?" + urlencode({k: v[0] for k, v in query.items()})
     async with semaphore:
         async with sess.get(url) as res:
-            with open(dest / f"{i:04}.png", "wb") as f:
+            with open(dest / f"{i:06}.png", "wb") as f:
                 f.write(await res.read())
+
+
+def is_first_url_valid(url: str):
+    return "/render/d-solo/" in url
 
 
 async def main():
@@ -46,29 +60,53 @@ async def main():
     print()
 
     print("Url de la première image :")
-    first = urlparse(input(">> "))
-    query = dict(parse_qs(first.query))
+    raw_url = ""
+    while True:
+        raw_url = input(">> ").strip()
+
+        if is_first_url_valid(raw_url) and URL_REGEX.fullmatch(raw_url):
+            break
+
+        print("L'url n'est pas valide, veuillez en donner une autre.")
+        print("(Cela doit être l'url du rendu de la première image).")
+
+    first_url = urlparse(raw_url)
+    query = dict(parse_qs(first_url.query))
     first_date = query["var-day"][0]
+    first_date_obj = datetime.strptime(first_date, "%Y-%m-%d")
     
     print()
-    print(f"La première date est {first_date}, donnez celle de fin (même format) :")
-    last_date = input(">> ")
+    print(f"La date de début est {first_date}, donnez celle de fin (même format) :")
+    last_date = ""
+    while True:
+        last_date = input(">> ")
+
+        if not VALID_DATE_REGEX.fullmatch(last_date):
+            print("Respectez le format de la date svp.")
+            continue
+
+        last_date_obj = datetime.strptime(last_date, "%Y-%m-%d")
+            
+        if first_date_obj > last_date_obj:
+            print("La date de fin ne peut pas être antérieure à celle de début.")
+            continue
+
+        break
 
     all_dates = daterange(
-        datetime.strptime(first_date, "%Y-%m-%d"),
-        datetime.strptime(last_date, "%Y-%m-%d"),
+        first_date_obj,
+        last_date_obj,
     )
 
     with TemporaryDirectory() as tmp:
         async with aiohttp.ClientSession() as sess:
             print("Génération des images...")
-            await tqdm_asyncio.gather(*[download_image(sess, first, query.copy(), i, d, Path(tmp)) for i, d in enumerate(all_dates)])
+            await tqdm_asyncio.gather(*[download_image(sess, first_url, query.copy(), i, d, Path(tmp)) for i, d in enumerate(all_dates)])
 
         proc = await asyncio.create_subprocess_shell(
-            f"ffmpeg -y -r 10 -f image2 -i \"{tmp}/%04d.png\" -vcodec libx264 -crf 10 -pix_fmt yuv420p \"{here / 'out.mp4'}\""
+            f"ffmpeg -y -r 10 -f image2 -i \"{tmp}/%06d.png\" -vcodec libx264 -crf 10 -pix_fmt yuv420p \"{here / 'out.mp4'}\""
         )
 
         await proc.communicate()
-
 
 asyncio.run(main())
