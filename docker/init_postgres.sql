399181367cc7bd7d75a505d2a50ef184de6f9e38
Raphaël C <raphael.caldwell@etu.unice.fr>
2023-06-30 09:21:00 +0200

    Donner les droits après que le schema à été importé

--- docker/init_postgres.sql
+++ docker/init_postgres.sql
@@ -6,6 +6,15 @@ create database quality_check_data;
 -- connect to the database
 \c quality_check_data
 
+-- import the schema
+\i /docker-entrypoint-initdb.d/schema.sql0
+
+-- import initial data
+\i /docker-entrypoint-initdb.d/inserts.sql0
+
+-- create the indexes
+\i /docker-entrypoint-initdb.d/create_indexes.sql0
+
 -- create the user for grafana
 revoke all on schema public from public;
 
@@ -16,12 +25,3 @@ create user grafana_reader with password 'grafana';
 grant connect on database quality_check_data to grafana_reader;
 grant usage on schema public to grafana_reader;
 grant select on all tables in schema public to grafana_reader;
-
--- import the schema
-\i /docker-entrypoint-initdb.d/schema.sql0
-
--- import initial data
-\i /docker-entrypoint-initdb.d/inserts.sql0
-
--- create the indexes
-\i /docker-entrypoint-initdb.d/create_indexes.sql0
